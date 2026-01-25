eda4d8f6ae4126f227515d564e9e7fa6294e6411
JuliaChen <JuliaChenCS@gmail.com>
2026-01-25 00:01:05 -0800

    + ready to merge

--- mini-amazon-backend/src/models/user.model.ts
+++ mini-amazon-backend/src/models/user.model.ts
@@ -3,9 +3,9 @@ import { Role } from "../types/role.enum.js";
 import { model, Schema } from "mongoose";
 
 const userSchema = new Schema<UserI>({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: Object.values(Role), default: Role.User },
-});
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: Object.values(Role), default: Role.User }
+})
 
-export const User = model<UserI>("User", userSchema);
+export const User = model<UserI>('User', userSchema);
